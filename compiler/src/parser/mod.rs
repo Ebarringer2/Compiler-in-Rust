c38fa4e4eb06d9e2a9dd0f425550e5974baac687
Elliott Barringer <elliottbarringer@icloud.org>
2024-01-07 18:08:45 -0800

    working on fixing issues in the parsing process

--- compiler/src/parser/mod.rs
+++ compiler/src/parser/mod.rs
@@ -2,10 +2,11 @@ use crate::lexer::Token;
 
 pub struct Parser<'a> {
     tokens: &'a [Token],
-    current: usize
+    current: usize,
+    ast: Vec<Expr>
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub enum Expr {
     Number(i64),
     BinOp(Box<Expr>, Token, Box<Expr>)
@@ -13,27 +14,40 @@ pub enum Expr {
 
 impl<'a> Parser<'a> {
     pub fn new(tokens: &'a [Token]) -> Self {
-        Parser { tokens, current: 0}
+        Parser {
+            tokens,
+            current: 0,
+            ast: Vec::new()
+        }
     }
-    fn parse_number(&mut self) -> Expr {
-        if let Token::Number(value) = &self.tokens[self.current] {
+    fn parse_number(&mut self, ast: &mut Vec<Expr>) -> Expr {
+        if let Some(&Token::Number(value)) = self.tokens.get(self.current) {
             self.current += 1;
-            Expr::Number(*value)
+            let expr: Expr = Expr::Number(value);
+            ast.push(expr.clone());
+            expr
         } else {
             panic!("Expected a number")
         }
     }
-    fn parse_term(&mut self) -> Expr {
-        let mut left_expr: Expr = self.parse_number();
-        while let Token::Multiply | Token::Plus = &self.tokens[self.current] {
-            let operator: Token = self.tokens[self.current].clone();
-            self.current += 1;
-            let right_expr: Expr = self.parse_number();
-            left_expr = Expr::BinOp(Box::new(left_expr), operator, Box::new(right_expr));
+    fn parse_term(&mut self, ast: &mut Vec<Expr>) {
+        self.parse_number(ast);
+        while let Some(&token) = self.tokens.get(self.current) {
+            match token {
+                Token::Plus | Token::Minus | Token::Multiply | Token::Divide => {
+                    self.current += 1;
+                    let temp_expr: Expr = ast.pop().unwrap();
+                    let right_expr: Expr = self.parse_number(ast);
+                    let operator: Token = token.clone();
+                    ast.push(Expr::BinOp(Box::new(temp_expr), operator, Box::new(right_expr)));
+                }
+                _ => break
+            }
         }
-        return left_expr
     }
-    pub fn parse(&mut self) -> Expr {
-        self.parse_term()
+    pub fn parse(&mut self) {
+        let mut ast: Vec<_> = Vec::new();
+        self.parse_term(&mut ast);
+        println!("Parsed AST: {:?}", self.ast);
     }
 }
